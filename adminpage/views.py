b802818a9137a3731a7a5b7ac5f0bf7a09064008
B-nod <binodtamang245@gmail.com>
2024-07-11 21:37:25 +0545

    admin

--- adminpage/views.py
+++ adminpage/views.py
@@ -6,7 +6,7 @@ def admin_page(request):
     return render(request, 'admins/dashboard.html')
 
 def bloglist(request):
-    blog = Blog.objects.all()
+    blog = Blog.objects.all()[:10]
     context = {
         'blog':blog
     }
