0bca382d55160badd7d1c374b1bb190ed0601f39
B-nod <binodtamang245@gmail.com>
2024-07-11 21:46:38 +0545

    admin

--- adminpage/views.py
+++ adminpage/views.py
@@ -1,13 +1,20 @@
 from django.shortcuts import render
 from home.models import *
+from django.core.paginator import Paginator
 
 # Create your views here.
 def admin_page(request):
     return render(request, 'admins/dashboard.html')
 
 def bloglist(request):
-    blog = Blog.objects.all()[:10]
+    blog = Blog.objects.all().order_by('-id')
+    paginator = Paginator(blog, 10)
+    page_number = request.GET.get(page_number)
+    page_obj = paginator.get_page(page_number)
+    page_range = range(1, paginator.num_pages +1)
     context = {
-        'blog':blog
+        'blog':blog,
+        'page_obj':page_obj,
+        'page_range':page_range
     }
     return render(request, 'admins/bloglist.html', context)
