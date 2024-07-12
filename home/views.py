6e835fb0e401414560a96d1abdfe83ed19ca3115
B-nod <binodtamang245@gmail.com>
2024-07-12 22:25:35 +0545

    blog

--- home/views.py
+++ home/views.py
@@ -1,5 +1,12 @@
 from django.shortcuts import render
+from . models import *
 
 # Create your views here.
 def homepage(request):
-    return render(request, 'home/index.html')
+    recent_blog = Blog.objects.all().order_by('-id')[:1]
+    blogs = Blog.objects.all()
+    context = {
+        'recent':recent_blog,
+        'blogs':blogs
+    }
+    return render(request, 'home/index.html', context)
