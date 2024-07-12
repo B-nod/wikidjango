1ae2b7e56c7f7cfe359d7d1ef13ca2e36ff99a1e
B-nod <binodtamang245@gmail.com>
2024-07-12 23:37:33 +0545

    addblog

--- adminpage/views.py
+++ adminpage/views.py
@@ -7,7 +7,6 @@ def admin_page(request):
     return render(request, 'admins/dashboard.html')
 
 def bloglist(request):
-  
     blog_list = Blog.objects.all().order_by('-id')
     paginator = Paginator(blog_list, 20)  # Show 20 blogs per page
 
@@ -15,7 +14,6 @@ def bloglist(request):
     page_obj = paginator.get_page(page_number)
 
     context = {
-        'blog':blog_list,
         'page_obj': page_obj,
     }
     return render(request, 'admins/bloglist.html', context)
